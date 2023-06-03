551a8eafd8d525322fa4142e98393a022185f7cb
PixelTux <luis.pixeltux@gmail.com>
2023-06-03 20:03:59 +0100

    Small fixes

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,7 +3,10 @@
     <div class="container flex justify-center">
         
         <div class="flex-col items-center">
-            <div class="text-center font-permarker text-xl xl:text-2xl ">PixelTux</div>
+            <div class="text-center font-permarker text-xl xl:text-2xl ">{{ config('app.name')}}</div>
+            <div class="text-[0.5rem] text-center">
+                @lang('All rights reserved ') © {{ date('Y') }}
+            </div>
         </div>
     </div>
 </footer>
